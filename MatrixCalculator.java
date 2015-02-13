12109dc594315fe313087d6fd493a9479f8b2c90
Siddarth Senthilkumar <ssenthilkumar3@gatech.edu>
2015-02-13 10:52:15 -0500

    Created Constructor and Matrix object

--- MatrixCalculator.java
+++ MatrixCalculator.java
@@ -1,3 +1,14 @@
+/**
+ * Calculates various operations on matrices
+ * and vectors.
+ * @author Siddarth Senthilkumar
+ * @version 1.0
+ */
 public class MatrixCalculator {
 	
+	double[][] matrixA;
+
+	public MatrixCalculator(int m, int n) {
+		matrixA = new double[m][n];
+	}
 }
