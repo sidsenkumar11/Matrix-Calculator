4ab302994b9d04319385b58f41ebf9af3140c851
ashika789 <AshikaGanesh@lawn-143-215-49-241.lawn.gatech.edu>
2015-03-25 19:47:54 -0400

    Added JAMA package and created new Hilbert.java

---
+++ Hilbert.java
@@ -0,0 +1,32 @@
+package hilbert;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import Jama.Matrix;
+import Jama.util.Maths;
+
+public class Hilbert
+{
+	private double[][] array;
+	private Matrix matrix,l,u,b,q,r;
+	private int size;
+	
+	
+	public Hilbert(int columns) {
+		size = columns;
+		b = new Matrix(columns,1,1);
+		array=new double[columns][columns];
+		for(int i=0;i<array.length;i++)
+			for(int j=0;j<array[i].length;j++)
+				array[i][j]=(1.0/(i+j+1));
+		matrix=new Matrix(array);
+	}
+	
+	public Hilbert(double[][] array) {
+		size=array.length;
+		b=new Matrix(size,1,1);
+		this.array=array;
+		matrix=new Matrix(array);
+	}
+
+}
