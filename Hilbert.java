8274652e32a4b4790fcab4853e0fe61372458265
ashika789 <AshikaGanesh@lawn-143-215-49-241.lawn.gatech.edu>
2015-03-25 19:50:43 -0400

    Updated formatting

--- Hilbert.java
+++ Hilbert.java
@@ -5,28 +5,34 @@ import java.util.Arrays;
 import Jama.Matrix;
 import Jama.util.Maths;
 
-public class Hilbert
-{
+/**
+ * Hilbert Matrix Procedures
+ * 
+ * @author Ashika Ganesh
+ * @version 1.0
+ */
+public class Hilbert {
 	private double[][] array;
-	private Matrix matrix,l,u,b,q,r;
+	private Matrix matrix, l, u, b, q, r;
 	private int size;
-	
-	
+
 	public Hilbert(int columns) {
 		size = columns;
-		b = new Matrix(columns,1,1);
-		array=new double[columns][columns];
-		for(int i=0;i<array.length;i++)
-			for(int j=0;j<array[i].length;j++)
-				array[i][j]=(1.0/(i+j+1));
-		matrix=new Matrix(array);
+		b = new Matrix(columns, 1, 1);
+		array = new double[columns][columns];
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				array[i][j] = (1.0 / (i + j + 1));
+			}
+		}
+		matrix = new Matrix(array);
 	}
-	
+
 	public Hilbert(double[][] array) {
-		size=array.length;
-		b=new Matrix(size,1,1);
-		this.array=array;
-		matrix=new Matrix(array);
+		size = array.length;
+		b = new Matrix(size, 1, 1);
+		this.array = array;
+		matrix = new Matrix(array);
 	}
 
 }
