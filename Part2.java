dff8942f93219f69602ac504bc0758f23ae2bdec
Katherine Cabezas <kcabezas@gatech.edu>
2015-03-28 16:49:04 -0400

    Working on part 2

--- Part2.java
+++ Part2.java
@@ -1,1 +1,33 @@
+/**
+ * (Part 2 - work in progress)
+ *
+ * @author Katherine Cabezas
+ * @version 1.0
+ */
+public class Part2 {
 
+    /**
+     * Method for jacobi iteration.
+     * @param a Matrix n x n
+     * @param y Vector n x 1
+     * @param x Initial guess vector
+     * @param tol Error tolerance number
+     * @return number of iterations required to reach tolerance
+     */
+    public static int jacobi(Matrix a, Vector y, Vector x, double tol) {
+
+    }
+
+    /**
+     * Method for jacobi iteration.
+     * @param a Matrix n x n
+     * @param y Vector n x 1
+     * @param x Initial guess vector
+     * @param tol Error tolerance number
+     * @return number of iterations required to reach tolerance
+     */
+    public static int gauss_seidel(Matrix a, Vector y, Vector x, double tol) {
+
+    }
+
+}
