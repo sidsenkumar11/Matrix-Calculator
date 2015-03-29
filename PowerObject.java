fdde872ba950ad197fd8b14514597fe0923faf34
Siddarth Senthilkumar <ssenthilkumar3@gatech.edu>
2015-03-29 13:45:40 -0400

    Created PowerObject

---
+++ PowerObject.java
@@ -0,0 +1,60 @@
+import java.math.BigDecimal;
+
+/**
+ * A representation of a given power_method call.
+ * 
+ * @author Siddarth Senthilkumar
+ * @version 1.0
+ */
+public class PowerObject {
+	private BigDecimal approximatedEigenvalue;
+	private Vector approximatedEigenvector;
+	private int numIterations;
+	private boolean converges;
+
+	/**
+	 * Constructs a power method object.
+	 */
+	public PowerObject(BigDecimal approximatedEigenvalue, Vector approximatedEigenvector, int numIterations, boolean converges) {
+		this.approximatedEigenvalue = approximatedEigenvalue;
+		this.approximatedEigenvector = approximatedEigenvector;
+		this.numIterations = numIterations;
+		this.converges = converges;
+	}
+
+	public boolean converges() {
+		return converges;
+	}
+
+	public BigDecimal getEigenvalue() {
+		return approximatedEigenvalue;
+	}
+
+	public Vector getEigenvector() {
+		return approximatedEigenvector;
+	}
+
+	public int getIterations() {
+		return numIterations;
+	}
+
+	public void setConverges(boolean converges) {
+		this.converges = converges;
+	}
+
+	public void setEigenvalue(BigDecimal value) {
+		this.approximatedEigenvalue = value;
+	}
+
+	public void setEigenvector(Vector vector) {
+		this.approximatedEigenvector = vector;
+	}
+
+	public void setIterations(int iterations) {
+		this.numIterations = iterations;
+	}
+
+	public void incrementIterations() {
+		this.numIterations++;
+	}
+}
