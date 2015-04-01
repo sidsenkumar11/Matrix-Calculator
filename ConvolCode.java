1b0fe3ab371f5b509a1590d6857581a764969e56
Katherine Cabezas <kcabezas@gatech.edu>
2015-03-31 23:13:27 -0400

    Formatting main method

--- ConvolCode.java
+++ ConvolCode.java
@@ -1,9 +1,10 @@
 import java.util.Random;
+import java.util.Scanner;
 /**
  * Program to create convolutional code word for
  * binary data streams.
  * @author Katherine Cabezas
- * @version 1.0
+ * @version 2.0
  */
 public class ConvolCode {
 
@@ -152,56 +153,10 @@ public class ConvolCode {
     }
 
     public static void main(String[] args) {
-        double[] stream = { 1, 0, 1 };
-        ConvolCode coder = new ConvolCode(stream);
-        String[] y = coder.encode();
+        System.out.println("------------------------------------------------");
+        System.out.println("Welcome to the Convolutional Code program!");
+        System.out.println("What would you like to do?");
+        System.out.println("------------------------------------------------");
 
-        System.out.println("Convolutional code word: ");
-        for (String s : y) {
-            System.out.print(s + " ");
-        }
-        System.out.println();
-
-        double[] x = {0, 0, 0};
-        Vector x_guess = new Vector(x);
-        double tol = Math.pow(10, -8);
-        System.out.println(coder.getMatrixA0());
-        System.out.println(coder.getY0());
-        // Vector x_final = jacobi.j_decode(coder.getMatrixA0(), coder.getY0(), x_guess, tol);
-        // System.out.println(x_final);
-        // double[][] aMatrix = {
-        //     { 5, -2, 3 },
-        //     { -3, 9, 1 },
-        //     { 2, -1, -7 }
-        // };
-        // double[] yValues = { -1, 2, 3 };
-        // double[] xValues = { 0, 0, 0 };
-        //
-        // Matrix a = new Matrix(3, 3);
-        // Vector y = new Vector(3);
-        // Vector x = new Vector(3);
-        // double tolerance = Math.pow(10, -4);
-        //
-        // for (int i = 0; i < a.rows(); i++) {
-        //     for (int j = 0; j < a.columns(); j++) {
-        //         a.set(i, j, aMatrix[i][j]);
-        //     }
-        // }
-        //
-        // for (int i = 0; i < y.rows(); i++) {
-        //     y.set(i, yValues[i]);
-        // }
-        //
-        // for (int i = 0; i < x.rows(); i++) {
-        //     x.set(i, xValues[i]);
-        // }
-        //
-        // System.out.println("Jacobi:");
-        // Vector iterations = jacobi.jacobi(a, y, x, tolerance);
-        // System.out.println(iterations);
-        //
-        // System.out.println("\nGauss-Seidel:");
-        // Vector iterations2 = gauss_seidel.gauss_seidel(a, y, x, tolerance);
-        // System.out.println(iterations2);
     }
 }
